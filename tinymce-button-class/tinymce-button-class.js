c2a536863e913b492ceac682974f220174c3fc2a
michaelLsmith <mlsmith45@gmail.com>
2017-09-07 09:19:57 -0400

    changed URL Variable for live/stanging

--- tinymce-button-class/tinymce-button-class.js
+++ tinymce-button-class/tinymce-button-class.js
@@ -1,14 +1,11 @@
 (function() {
-  var base_url = window.location.origin;
+  var baseUrl = window.location.origin;
   var host = window.location.host;
   var pathArray = window.location.pathname.split( '/' );
   console.log(base_url, host, pathArray);
   var wpURL = base_url + '/' + pathArray[1];
-  // console.log(wpURL);
+  // on Bitnami localhost, need to use wpURL
   //for staging and live site just need base_url
-
-
-
     tinymce.PluginManager.add( 'button_class',
       function( editor, url ) {
         editor.addButton('button_class', {
@@ -19,10 +16,8 @@
           function() {
             //Insert selected text back into editor, wrapping it in an anchor tag
             editor.execCommand('mceInsertContent', false,
-          `<a href="${wpURL}/donate"><button type="button" class="btn-purple">Donate</button></a>`
+          `<a href="${baseUrl}/donate"><button type="button" class="btn-purple">Donate</button></a>`
         );
           });
       });
 })();
-
-//'wpURL +'/donate
